a5420b27cc925b6555318e64ef7c1ed0498e43cf
DamilolaOladipupo <akinsoladamilola29@gmail.com>
2023-08-03 05:20:40 -0700

    implements hamburger functionality

--- starter/main.js
+++ starter/main.js
@@ -1,4 +1,10 @@
 var hamburger = document.querySelector(".navmobile-menu")
+var mobileLinksHolder = document.querySelector(".navmobile-links")
+var backdrop = document.querySelector(".backdrop")
+
 function hamburgerHandler(){
     hamburger.classList.toggle("showmenu")
+    mobileLinksHolder.classList.toggle("shownavmobile-links")
+    backdrop.classList.toggle("showbackdrop")
+
 }
