6d8bcc40aa21a349e5a83eebb10e0f36a70362f5
DamilolaOladipupo <akinsoladamilola29@gmail.com>
2023-08-16 06:10:53 -0700

    completed desktop and  mobile devices view

--- starter/main.js
+++ starter/main.js
@@ -1,10 +1,28 @@
-var hamburger = document.querySelector(".navmobile-menu")
-var mobileLinksHolder = document.querySelector(".navmobile-links")
-var backdrop = document.querySelector(".backdrop")
+var hamburger = document.querySelector(".navmobile-menu");
+var mobileLinksHolder = document.querySelector(".navmobile-links");
+var backdrop = document.querySelector(".backdrop");
 
-function hamburgerHandler(){
-    hamburger.classList.toggle("showmenu")
-    mobileLinksHolder.classList.toggle("shownavmobile-links")
-    backdrop.classList.toggle("showbackdrop")
-
+function hamburgerHandler() {
+  hamburger.classList.toggle("showmenu");
+  mobileLinksHolder.classList.toggle("shownavmobile-links");
+  backdrop.classList.toggle("showbackdrop");
 }
+
+ScrollReveal({
+  reset: true,
+  distance: "100px",
+  origin: "right",
+  duration: 1800,
+});
+
+ScrollReveal().reveal(".header-banner__content h1", {
+  origin: "left",
+});
+ScrollReveal().reveal(".aboutfoods-text h2, aboutfoods-text h2", {
+  origin: "right",
+});
+ScrollReveal().reveal(".foodcard img", {
+  rotate: {
+    x: 180,
+  },
+});
